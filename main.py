dc46bf2563a71c63880cbd5dbf5a7b45561f22bb
Mithileshseesunkur <zavallingum@gmail.com>
2024-07-26 17:28:15 +0200

    goodtogo

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model=YOLO("yolov8m.yaml")
+model=YOLO("yolov10m.yaml")
 
 
 pathCleanedDataset=r"C:\Users\mitarbeiter\PycharmProjects\YOLO_training_MA\model.yaml"
 pathModel=r"C:\Users\mitarbeiter\PycharmProjects\model"
 results=model.train(data=pathCleanedDataset,resume=True,
-                    name="MA_model", project=pathModel,save=True)
+                    name="MA_model", project=pathModel,save=True,device=1)
